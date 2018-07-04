33148005158fcd5a7df3d67592f8103d9581e1d9
tarou.git <hdaliu1@gmail.com>
2018-07-04 12:18:40 +0000

    tasklist 4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,13 +30,21 @@ class TasksController < ApplicationController
   def update
     @tasks = Task.find(params[:id])
 
-    if @tasks.update(message_params)
+    if @tasks.update(tasks_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @tasks
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
+  end
+  
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
 
 end
