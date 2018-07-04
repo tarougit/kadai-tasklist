ebce69716b690ffc3a875dbbc7a7d75fa0783dbe
tarou.git <hdaliu1@gmail.com>
2018-07-04 11:35:10 +0000

    tasklist 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,9 +10,28 @@ class TasksController < ApplicationController
   def new
     @tasks = Task.new
   end
+  
+  def create
+    @tasks = Task.new(tasks_params)
+
+    if @tasks.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
 
   def edit
     @tasks = Task.find(params[:id])
   end
 
 end
+
+private
+
+  # Strong Parameter
+  def tasks_params
+    params.require(:task).permit(:content)
+  end
