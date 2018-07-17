4b778005539bf9e7ead58f5755fb51a0218df122
tarou.git <hdaliu1@gmail.com>
2018-07-17 12:41:19 +0000

    post3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:create]
   before_action :correct_user, only: [:destroy]
   
   def create
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true, length: { maximum: 255 }
 end
