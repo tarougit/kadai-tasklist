34f34caf05e0bf4bffae302b9c97bd58f014ef73
tarou.git <hdaliu1@gmail.com>
2018-07-23 11:35:55 +0000

    post5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:create]
-  before_action :correct_user, only: [:update]
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
